469d5e515a6ab663b257a968085355500671d0e0
Goganna <80478289+Goganna@users.noreply.github.com>
2022-02-07 23:02:57 +0200

    Added css tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 const gulp = require('gulp');
 const terser = require('gulp-terser');
+const plumber = require('gulp-plumber')
+const sourcemaps = require('gulp-sourcemaps')
+const csso = require('gulp-csso')
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
 
 const html = function() {
    return gulp.src('./source/index.html').pipe(gulp.dest('./build/'));
 }
 const js = function() {
-    return gulp.src('./source/js/script.js')
+    return gulp.src('source/js/script.js')
         .pipe(terser())
         .pipe(gulp.dest('./build'))
 }
+function css () {
+    return src('source/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(rename('style.min.css'))
+        .pipe(sourcemaps.write("./"))
+        .pipe(dest('build/css'))
+}
 exports.html = html
+exports.css = css
 exports.js = js
 
