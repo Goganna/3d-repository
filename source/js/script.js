fa9bf2284af021cd4845e543c20612a1eb5ca9d5
Goganna <80478289+Goganna@users.noreply.github.com>
2022-02-21 23:47:33 +0200

    Added price section

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,15 @@
 let menuBtn = document.querySelector('.menu__btn');
-console.log(menuBtn);
+/*console.log(menuBtn);*/
 let menu = document.querySelector('.menu__body');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
+})
+
+const acc = document.querySelector('price__info--title')
+const accPanel = document.querySelector('.price__info')
+
+acc.addEventListener("click", function(){
+  accPanel.classList.toggle("active")
 })
 
